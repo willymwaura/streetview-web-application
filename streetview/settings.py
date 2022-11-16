1f9aee18cb911f1af0225f16d28f6acfdccf47b1
willymwaura <wilsonmwaura697@gmail.com>
2022-11-16 09:49:48 +0300

    staticfiles

--- streetview/settings.py
+++ streetview/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-zxag74(6%jzp8*$z0x%6w**)(eo-4$7k+k88=b_a_c%58*&8!9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 CORS_ALLOWED_ORIGINS=['*']
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -124,6 +125,7 @@ STATIC_ROOT=os.path.join(BASE_DIR,"statifiles")
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS=os.path.join(BASE_DIR,"static"),
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
